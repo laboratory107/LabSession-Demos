6ea3f0c77ec9b91caeb335512a822b91b01fa25e
Lovro Posarić <lovro.posaric@gmail.com>
2022-11-02 13:29:31 +0100

    Add JTAG debugging project

---
+++ Debugging/JTAG/src/main.cpp
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+#include <map>
+#include <ArduinoTrace.h>
+
+int led = 21;
+std::map<String, int> cmds = {
+  {"on", HIGH},
+  {"off", LOW}
+};
+
+void greeting() {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(1000);
+
+    //DUMP(i);
+    //BREAK();
+  }
+}
+
+void processCommand(String command)
+{
+   // TRACE();
+    Serial.println("You asked for light to be: " + command);
+    if(command == "on" || command == "off")
+    {
+      Serial.println("light is now " + command);
+      digitalWrite(led, cmds[command]);
+    }
+    else
+    {
+      Serial.println("I don't get that :(");
+    }
+}
+
+void readSerialInput()
+{
+  String input = "";
+
+  while(Serial.available() > 0)
+  {
+    byte incoming = Serial.read();
+    input.concat((char)incoming);
+  }
+
+  if(input != "")
+  {
+    input.trim();
+    processCommand(input);
+    input = "";
+  }
+}
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println("Light bringer v1.0");
+  greeting();
+  Serial.println("What is it you wish to do?");
+}
+
+void loop()
+{
+  readSerialInput();
+}
