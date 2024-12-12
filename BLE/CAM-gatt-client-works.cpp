efffb1672d00fb2daf24d5491515c351b9b334fc
lukamilicfoi <71666635+lukamilicfoi@users.noreply.github.com>
2024-12-13 00:22:06 +0100

    Update CAM-gatt-client-works.cpp

--- BLE/CAM-gatt-client-works.cpp
+++ BLE/CAM-gatt-client-works.cpp
@@ -3,30 +3,22 @@
 #include "BLEDevice.h"
 #include "BLE2902.h"
 
+BLEAdvertisedDevice ad;
+bool connect = false;
 BLEUUID serviceUUID("91bad492-b950-4226-aa2b-4ede9fa42f59");
 BLEUUID    charUUID("0d563a58-196a-48ce-ace2-dfec78acc814");
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertisedDevice) {
     if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
       advertisedDevice.getScan()->stop();
-      BLEClient*  pClient  = BLEDevice::createClient();
-      pClient->connect(&advertisedDevice);
-      BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
-      if (pRemoteService == nullptr) {
-        return;}
-      BLERemoteCharacteristic* pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
-      if (pRemoteCharacteristic == nullptr) {
-        return;}
-      Serial.println(pRemoteCharacteristic->readValue().c_str());
-      pRemoteCharacteristic->writeValue("abcd");
-      pClient->disconnect();
+      ad = advertisedDevice;
+      connect = true;
     }
   }
 };
 
 void setup() {
   Serial.begin(115200);
-
   BLEDevice::init("");
   BLEScan *pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
@@ -34,4 +26,18 @@ void setup() {
   pBLEScan->start(100);
 }
 
-void loop() { }
+void loop() {
+  while (!connect) { delay(100); }
+  BLEClient*  pClient  = BLEDevice::createClient();
+  pClient->connect(&ad);
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    return;}
+  BLERemoteCharacteristic* pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+  if (pRemoteCharacteristic == nullptr) {
+    return;}
+  Serial.println(pRemoteCharacteristic->readValue().c_str());
+  pRemoteCharacteristic->writeValue("abcd");
+  pClient->disconnect();
+  delay(1000000);
+}
